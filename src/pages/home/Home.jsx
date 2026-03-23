ebf98b462758dad95d0105537563ee56937ed854
DVOanh <dvoanh25062004@gmail.com>
2026-03-23 23:23:44 +0700

    Sua them danh muc

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,35 @@
-import Product from "../../component/product/Product"; 
+import Product from "../../component/product/Product";
 import Banner from '../../component/banner/Banner';
 
 import Footer from "../../component/footer/Footer";
 import './home.css';
-function Home(){
-    return(
+import { useState, useEffect } from "react";
+function Home() {
+    const [danhmuc, setDanhmuc] = useState([]);
+    useEffect(() => {
+        fetch('https://backend-production-f0ff.up.railway.app/categories')
+            .then(res => res.json())
+            .then(data => setDanhmuc(data));
+    }, []);
+    return (
         <div className="container">
-            <div className="banner">
-                <Banner/>
+            <div className="banner_danhmuc">
+                <div className="banner">
+                    <Banner />
+                </div>
+                <div className="danhmuccontainer">
+                    <h1 className="tieude_danhmuc">DANH MỤC</h1>
+                    <div className="danhmuclist">
+                        
+                            {
+                                danhmuc.map(item=>(
+                                    <div className="danhmucitem">{item.name}</div>
+                                ))
+                            }
+                        </div>
+                </div>
             </div>
-            <Product/>
+            <Product />
         </div>
     )
 }
