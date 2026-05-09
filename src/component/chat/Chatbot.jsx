8738032fd4682df1b79e08cd492dd37ad065d0fa
DVOanh <dvoanh25062004@gmail.com>
2026-05-09 09:12:14 +0700

    Chat bot

--- src/component/chat/Chatbot.jsx
+++ src/component/chat/Chatbot.jsx
@@ -1,39 +1,65 @@
-import { useState, useEffect, useRef} from "react";
-import "./chatbot.css"
+import { useState, useEffect, useRef } from "react";
+import "./chatbot.css";
 function ChatBot() {
     const [openChatbot, setOpenchatbot] = useState(false);
     const [openiconchatbot, setopeniconchatbot] = useState(true);
     const [loinhan, setLoinhan] = useState("");
-    const [chat, setChat] = useState([
-        {
-            user: null,
-            bot: "Xin chào! Tôi sẵn sàng giúp đỡ bạn. Bạn cần gì?"
-        }
-    ]);
+    const [loading, setLoading] = useState(false);
+    const [loadingWelcome, setLoadingWelcome] = useState(true);
+    const [chat, setChat] = useState([]);
     const endRef = useRef(null);
 
     // Hàm scroll
-  const scrollToBottom = () => {
-    endRef.current?.scrollIntoView({
-      behavior: "smooth"
-    });
-  };
+    const scrollToBottom = () => {
+        endRef.current?.scrollIntoView({
+            behavior: "smooth",
+        });
+    };
 
-  // Khi chat thay đổi
-  useEffect(() => {
-    scrollToBottom();
-  }, [chat]);
+    // Khi chat thay đổi
+    useEffect(() => {
+        scrollToBottom();
+    }, [chat]);
     function openchatbot() {
         setOpenchatbot(true);
-        setopeniconchatbot(false)
+        setopeniconchatbot(false);
     }
     function closechatbot() {
-        setOpenchatbot(false)
-        setopeniconchatbot(true)
+        setOpenchatbot(false);
+        setopeniconchatbot(true);
     }
 
+    useEffect(() => {
+
+   setTimeout(() => {
+
+      setChat([
+         {
+            user: null,
+            bot: "Xin chào! Tôi sẵn sàng giúp đỡ bạn. Bạn cần gì?",
+         },
+      ]);
+
+      setLoadingWelcome(false);
+
+   }, 2000);
+
+}, []);
+
     const sendChat = async () => {
-        if (!loinhan.trim()){return};
+        if (!loinhan.trim()) {
+            return;
+        }
+        // thêm tin nhắn user ngay lập tức
+        setChat((prev) => [
+            ...prev,
+            {
+                user: loinhan,
+                bot: null,
+            },
+        ]);
+        setLoinhan("");
+        setLoading(true)
         try {
             const res = await fetch("https://backend-viv4.onrender.com/chatbot", {
                 method: "POST",
@@ -41,62 +67,84 @@ function ChatBot() {
                     "Content-Type": "application/json",
                 },
                 body: JSON.stringify({
-                    message: loinhan
+                    message: loinhan,
                 }),
             });
 
             const data = await res.json();
-            console.log(data)
-            setChat(prev => [...prev, { user: loinhan, bot: data?.reply }]);
-            setLoinhan("")
-        }
-        catch (err) {
+            console.log(data);
+            setChat((prev) => [...prev, { user: null, bot: data?.reply }]);
+            setLoading(false)
+        } catch (err) {
             console.error(err);
-            setChat(prev => [
+            setChat((prev) => [
                 ...prev,
-                { user: loinhan, bot: "⚠️ Lỗi server hoặc mạng" }
+                { user: loinhan, bot: "⚠️ Lỗi server hoặc mạng" },
             ]);
         }
-    }
+    };
 
     return (
         <>
-            {
-                openiconchatbot && (<div className="chatbot_parent" onClick={openchatbot}>
+            {openiconchatbot && (
+                <div className="chatbot_parent" onClick={openchatbot}>
                     <div className="ping"></div>
                     <img src="/chatbot.png" alt="" className="chatbot_child" />
                 </div>
-                )}
-            {
-                openChatbot && (
-                    <div className="chatbox">
-
-                        <div className="chat-header">
-                            <div className="cb"><div className="logochatbot"><img src="/chatbot.png" alt="" style={{ width: "100%" }} /></div><p>Trợ lý AI</p></div>
-                            <button onClick={closechatbot} className="closechatbot"><img src="/close.png" alt="" /></button>
+            )}
+            {openChatbot && (
+                <div className="chatbox">
+                    <div className="chat-header">
+                        <div className="cb">
+                            <div className="logochatbot">
+                                <img src="/chatbot.png" alt="" style={{ width: "100%" }} />
+                            </div>
+                            <p>Trợ lý AI</p>
                         </div>
-                        <div className="chat-content">
-                            {chat.map((c, i) => (
-                                <div key={i}>
-                                    {c.user && (
-                                        <div className="message user">{c.user}</div>
-                                    )}
-                                    <div className="message bot">{c.bot}</div>
-                                    {/* mốc cuối */}
-        <div ref={endRef}></div>
-                                </div>
-                            ))}
-                        </div>
-
-                        {/* INPUT */}
-                        <div className="chat-input">
-                            <input placeholder="Nhập tin nhắn..." value={loinhan} onChange={e => setLoinhan(e.target.value)} />
-                            <button onClick={sendChat}>Gửi</button>
-                        </div>
+                        <button onClick={closechatbot} className="closechatbot">
+                            <img src="/close.png" alt="" />
+                        </button>
                     </div>
-                )
-            }
+                    <div className="chat-content">
+                        {chat.map((c, i) => (
+                            <div key={i}>
+                                {c.user && <div className="message user">{c.user}</div>}
+                                {c.bot && <div className="message bot">{c.bot}</div>}
+
+                                {/* mốc cuối */}
+                                <div ref={endRef}></div>
+                            </div>
+                        ))}
+
+                        {loading && <div className="bot-loading">
+
+                            <img
+                                src="/chatbot.png"
+                                alt=""
+                                className="bot-avatar"
+                            />
+
+                            <div className="loading-bubble">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </div>
+
+                        </div>}
+                    </div>
+
+                    {/* INPUT */}
+                    <div className="chat-input">
+                        <input
+                            placeholder="Nhập tin nhắn..."
+                            value={loinhan}
+                            onChange={(e) => setLoinhan(e.target.value)}
+                        />
+                        <button onClick={sendChat}><img src="/send.png" alt="" /></button>
+                    </div>
+                </div>
+            )}
         </>
-    )
+    );
 }
 export default ChatBot;
